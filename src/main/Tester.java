5e39858c92ed08835789a43bc9b2df0c1506b354
omer <omer21104@gmail.com>
2021-10-19 18:24:02 +0300

    Changed hard coded paths in Tester class

--- src/main/Tester.java
+++ src/main/Tester.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,36 +13,58 @@ import encoder_decoder.AdaptiveHuffmanEncoderDecoder;
  */
 public class Tester 
 {
-	static final String[] in_comp = {"C:\\Users\\ASUS\\Dropbox\\Java\\Adaptive Huffman\\ExampleInputs\\OnTheOrigin.txt"};
-	static final String[] out_comp_path = {"C:\\Users\\ASUS\\Dropbox\\Java\\Adaptive Huffman\\test outputs\\"};
-	static final String[] out_decomp_path = {"C:\\Users\\ASUS\\Dropbox\\Java\\Adaptive Huffman\\test outputs\\"};
+	static final String sf_CurrentDirPath = System.getProperty("user.dir");
+	static final String sf_OutputDirName = "Test Outputs";
+
+	static final String sf_InCompFileName = "OnTheOrigin";
 	static final String comp_file_name = "Test";
 	static final String decomp_file_name = "OnTheOrigin";
 	static final String format = ".txt";
-	
+
+	private final String[] k_InCompPath;
+	private final String[] k_OutCompPath;
+	private final String[] k_OutDecompPath;
+
+	public Tester() throws Exception
+	{
+		// init dirs
+		if (!new File(sf_CurrentDirPath + "\\" + sf_OutputDirName).mkdirs())
+		{
+			throw new Exception("Folder already exists, delete it or rename it first");
+		}
+
+		String inCompFilePath = sf_CurrentDirPath + "\\ExampleInputs\\" + sf_InCompFileName + format;
+		k_InCompPath = new String[]{inCompFilePath};
+
+		k_OutCompPath = new String[]{sf_CurrentDirPath + "\\" + sf_OutputDirName};
+		k_OutDecompPath = k_OutCompPath;
+	}
+
 	/**
 	 * test a single file by compressing it and then decompressing it, comparing input and output.
-	 * <br> this method tries all possible symbol sizes allowed by the program implementation
+	 * <br> this method tries all possible symbol sizes allowed by the program implementation.
+	 * @param i_CleanupAfter a flag to indicate whether to delete residual files afterwards.
 	 */
-	public static void test()
+	public void test(boolean i_CleanupAfter)
 	{
+		System.out.println(k_OutCompPath[0]);
 		final int maxSupportedSymbolSize = 8;
 		AdaptiveHuffmanEncoderDecoder ende;
 		
 		for (int i = 1; i <= maxSupportedSymbolSize; i++) 
 		{
-			String[] out_comp_name = {out_comp_path[0] + comp_file_name + i};
-			String[] out_decomp_name = {out_decomp_path[0] + decomp_file_name + "_" + i + format};
+			String[] out_comp_name = {k_OutCompPath[0] + "\\" + sf_InCompFileName + i};
+			String[] out_decomp_name = {k_OutDecompPath[0] + "\\" + sf_InCompFileName + "_" + i + format};
 			
 			ende = new AdaptiveHuffmanEncoderDecoder(i);
-			ende.Compress(in_comp, out_comp_name);
+			ende.Compress(k_InCompPath, out_comp_name);
 			ende.Decompress(out_comp_name, out_decomp_name);
 			
 			// test result of compression
 			byte[] arr1 = null;
 			try 
 			{
-				arr1 = Files.readAllBytes(Path.of(in_comp[0]));
+				arr1 = Files.readAllBytes(Path.of(k_InCompPath[0]));
 			} 
 			catch (IOException e) 
 			{
@@ -60,5 +83,29 @@ public class Tester
 			boolean areFilesEqual = Arrays.equals(arr1, arr2);
 			System.out.println(String.format("%s", areFilesEqual ? "Success" : "Fail"));
 		}
+
+		if (i_CleanupAfter)
+		{
+			finalizeTest();
+		}
+	}
+
+	private void finalizeTest()
+	{
+		System.out.println("[*] Deleting residual files");
+		deleteDir(new File(k_OutCompPath[0]));
+	}
+
+	void deleteDir(File file)
+	{
+		File[] contents = file.listFiles();
+		if (contents != null) {
+			for (File f : contents) {
+				if (! Files.isSymbolicLink(f.toPath())) {
+					deleteDir(f);
+				}
+			}
+		}
+		file.delete();
 	}
 }
